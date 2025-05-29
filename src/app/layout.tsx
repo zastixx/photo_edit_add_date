979b5fa1dbebaa52c0e2b3704f1c49e91ba3add7
TARUN KUMAR UTTAM <57785497+zastixx@users.noreply.github.com>
2025-05-29 16:15:01 +0000

    name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Edit Phtoto Add Datastamp",
+  title: "Edit Photo Add Data-stamp",
   description: "Edit your photos and add a datastamp easily made by zastixx aka Tarun",
 };
 
